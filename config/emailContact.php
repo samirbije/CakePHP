a5b26fec9096df237cb21b67541e3f04821d3078
samirbije <samirbije@gmail.com>
2016-11-14 19:01:52 +0900

    Update emailContact.php

--- config/emailContact.php
+++ config/emailContact.php
@@ -1,12 +1,11 @@
 <?php
 $config = array(
-	// 
+	// eamil configuration 
 	'send' => array(
 		'subject'		=> 'Web tech test',
 		'to'			=> 'samir@otech.ne.jp',
 		'from'			=> 'samir@otech.ne.jp',
 		'from_name'		=> 'Tech Test',
 		'envelope_from'	=> NULL,
-	),
-	
+	),	
 );
